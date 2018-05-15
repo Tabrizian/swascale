2703f197d2460fdb3bec92045e21a433de6c0aaf
Iman Tabrizian <tabrizian@outlook.com>
2018-05-15 09:58:09 +0430

    Add configuration fetch method

---
+++ config/__init__.py
@@ -0,0 +1,23 @@
+import configparser
+
+
+class Config:
+
+    def __init__(self):
+        self.parser = configparser.ConfigParser()
+        self.parser.read('config/genorch.ini')
+
+    def __getattr__(self, name):
+        return self.parser[name]
+
+    """
+    Provides dict-style access to attributes
+    """
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+
+"""
+Always return an instance of Config
+"""
+cfg = Config()

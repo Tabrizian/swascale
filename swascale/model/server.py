bcf08b75132c075a114c17aa8ee9fa5dc4668029
Iman Tabrizian <tabrizian@outlook.com>
2018-05-22 22:18:14 +0430

    Improve server model

--- swascale/model/server.py
+++ swascale/model/server.py
@@ -2,8 +2,10 @@ from pymodm import MongoModel, fields
 
 
 class Server(MongoModel):
-    _id = fields.CharField(required=true)
-    name = fields.CharField(required=true)
-    image = fields.CharField(required=true)
-    flavor = fields.CharField(required=true)
-    networks = fields.FieldList(field=CharField)
+    uid = fields.CharField(required=True)
+    name = fields.CharField(required=True)
+    image = fields.CharField(required=True)
+    flavor = fields.CharField(required=True)
+    networks = fields.ListField(field=fields.CharField())
+    region = fields.CharField(required=True)
+    driver = fields.CharField(required=True)

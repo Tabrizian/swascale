dfa85e655fbbd58124ecb02f46d4667c40636faa
Iman Tabrizian <tabrizian@outlook.com>
2018-05-29 16:44:19 +0430

    Add server creation route

--- swascale/controllers/server.py
+++ swascale/controllers/server.py
@@ -1,14 +1,15 @@
-from flask import Blueprint, Response
+from flask import Blueprint, request, Response
 import json
 from bson.json_util import dumps
 
 import swascale.model.server as ServerModel
+from swascale.domain.server import Server
 
 
 server = Blueprint('server', 'server')
 
 
-@server.route('/', methods=['GET'])
+@server.route('', methods=['GET'])
 def index():
     response = Response(
         dumps(ServerModel.Server.objects.values().all()),
@@ -16,3 +17,19 @@ def index():
         mimetype='application/json'
         )
     return response
+
+
+@server.route('', methods=['POST'])
+def create():
+    print(request.json['region'])
+    server = Server(
+        name=request.json['name'],
+        image=request.json['image'],
+        networks=request.json['networks'],
+        region=request.json['region'],
+        driver=request.json['driver'],
+        flavor=request.json['flavor'],
+        key=request.json['key']
+        )
+    server.create()
+    return 'created'

8571d543ce113029109fcc92172b18f7dce8e1e8
Iman Tabrizian <tabrizian@outlook.com>
2018-06-05 12:38:55 +0430

    Improve server domain to findById

--- swascale/domain/server.py
+++ swascale/domain/server.py
@@ -2,6 +2,7 @@ from ..drivers.base import BaseDriver
 from swascale.utils.ansible import Ansible
 from . import db
 from bson.objectid import ObjectId
+import docker
 
 import click
 from os import system
@@ -9,17 +10,31 @@ from os import system
 
 class Server:
     def __init__(
-            self, name, image, flavor, region, driver, networks,
-            key=None
+            self=None, name=None, image=None,
+            flavor=None, region=None, driver=None, networks=None,
+            key=None, _id=None
             ):
-        self._id = None
-        self.name = name
-        self.flavor = flavor
-        self.region = region
-        self.image = image
-        self.networks = networks
-        self.key = key
-        self.driver = BaseDriver.get(driver)(region)
+        if _id is not None:
+            server = db.servers.find_one({'_id': ObjectId(_id)})
+            self._id = server['uid']
+            self.uid = _id
+            self.name = server['name']
+            self.flavor = server['flavor']
+            self.region = server['region']
+            self.image = server['image']
+            self.networks = server['networks']
+            if 'key' in server:
+                self.key = server['key']
+            self.driver = BaseDriver.get(server['driver'])(server['region'])
+        else:
+            self._id = None
+            self.name = name
+            self.flavor = flavor
+            self.region = region
+            self.image = image
+            self.networks = networks
+            self.key = key
+            self.driver = BaseDriver.get(driver)(region)
 
     @staticmethod
     def truncate():
@@ -58,7 +73,7 @@ class Server:
         Ansible.getInstance().execute_playbook(
             [self.ips[self.networks[0]][0]['addr']]
         )
-        db.servers.insert_one({
+        server = db.servers.insert_one({
             'uid': self._id,
             'name': self.name,
             'image': self.image,
@@ -67,6 +82,7 @@ class Server:
             'region': self.region,
             'driver': self.driver.name
         })
+        self.uid = server._id
 
     @staticmethod
     def delete(uid):
@@ -78,3 +94,16 @@ class Server:
     @property
     def ips(self):
         return self.driver.ips(self._id)
+
+    def swarm_init(self):
+        swarm_client = docker.DockerClient(
+            'tcp://' +
+            self.ips[self.networks[0]][0] +
+            ':' + cfg.docker['API_PORT']
+        ).swarm
+
+        swarm_client.init('eth0:' + cfg.docker['SWARM_PORT'],
+                          '0.0.0.0:' + cfg.docker['SWARM_PORT'])
+        print(swarm_client.attrs['JoinTokens'])
+        db.servers.update_one({_id: ObjectId(self.uid)},
+                              swarm_client.attrs['JoinTokens'])

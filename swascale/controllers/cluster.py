961754438d1b44fdf73606f91697f11ee4d9c06e
Iman Tabrizian <tabrizian@outlook.com>
2018-06-05 14:54:25 +0430

    Add support for cluster creation

--- swascale/controllers/cluster.py
+++ swascale/controllers/cluster.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, Response
 import json
 from bson.json_util import dumps
+from bson.objectid import ObjectId
 from swascale.domain import db
+from swascale.domain.server import Server
 
 cluster = Blueprint('cluster', 'cluster')
 
@@ -18,5 +20,21 @@ def index():
 
 @cluster.route('', methods=['POST'])
 def create():
-    print(request.json['vms'])
+    vm_manager = None
+    for vm in request.json.get('vms'):
+        if vm['role'] == 'manager':
+            print(vm)
+            vm_manager = vm
+            break
+    server = Server(_id=vm_manager['_id'])
+    server.swarm_init()
+
+    for vm in request.json.get('vms'):
+        if vm['_id'] != vm_manager['_id']:
+            if vm['role'] == 'manager':
+                manager = Server(_id=vm['_id'])
+                manager.swarm_join_manager(server)
+            elif vm['role'] == 'worker':
+                worker = Server(_id=vm['_id'])
+                worker.swarm_join_worker(server)
     return 'created'

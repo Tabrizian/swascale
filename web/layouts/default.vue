82b820bd180f4b98d2759577c3f1528f5292223b
Iman Tabrizian <tabrizian@outlook.com>
2018-06-14 15:09:12 +0430

    Add layout for web

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,100 +1,73 @@
 <template>
   <v-app dark>
     <v-navigation-drawer
-      :mini-variant.sync="miniVariant"
+      persistent
+      temporary
       :clipped="clipped"
-      v-model="drawer"
-      fixed
       app
-    >
+      v-model="drawer">
       <v-list>
-        <v-list-tile
-          router
-          :to="item.to"
-          :key="i"
-          v-for="(item, i) in items"
-          exact
-        >
+        <v-list-tile v-for='navItem in navItems'
+          :key='navItem.id'
+          :to='navItem.link'
+          :href='navItem.href'>
           <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
+            <v-icon>{{ navItem.icon }}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            <v-list-tile-title>{{ navItem.title }}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+    <v-toolbar app fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <span class="white--text">swascale</span>
+        <!-- <img src="/logo.png" height="30px"> -->
+      </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
+    <v-container fluid>
+      <nuxt />
+    </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
+    <v-footer height="auto">
+      <span class="caption white--text">&copy; Iman Tabrizian 2018</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
-      }
+export default {
+  mounted () {
+  },
+  data () {
+    return {
+      drawer: false,
+      clipped: true,
+      navItems: [
+        {
+          id: 1,
+          title: 'Home',
+          icon: 'home',
+          link: '/'
+        },
+        {
+          id: 2,
+          title: 'Servers',
+          icon: 'view_agenda',
+          link: '/servers'
+        },
+        {
+          id: 3,
+          title: 'Clusters',
+          icon: 'group_work',
+          link: '/clusters'
+        }
+      ]
     }
   }
+}
 </script>

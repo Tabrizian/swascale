cd3e5d52c4934ac07c74e216e73532a5a3bd51f4
Iman Tabrizian <tabrizian@outlook.com>
2018-06-14 16:28:31 +0430

    Add servers.vue file

---
+++ web/pages/servers.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-data-table :headers="headers" :items="servers" hide-actions class="elevation-1">
+        <template slot="items" slot-scope="props">
+          <td>{{ props.item.name }}</td>
+          <td>{{ props.item.uid }}</td>
+          <td>{{ props.item.image }}</td>
+          <td>{{ props.item.flavor }}</td>
+          <td>{{ props.item.region }}</td>
+          <td>{{ props.item.driver }}</td>
+        </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  async asyncData ({ app }) {
+    let servers = await app.$axios.$get('/api/server')
+    return { servers }
+  },
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Name',
+          value: 'name'
+        },
+        {
+          text: 'Universal ID',
+          value: 'uid'
+        },
+        {
+          text: 'Image Name',
+          value: 'image'
+        },
+        {
+          text: 'Flavor',
+          value: 'flavor'
+        },
+        {
+          text: 'Region',
+          value: 'region'
+        },
+        {
+          text: 'Driver',
+          value: 'driver'
+        }
+      ]
+    }
+  }
+}
+</script>
